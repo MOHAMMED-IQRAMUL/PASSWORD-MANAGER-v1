249a0c7cb7a1be70e0cac4bbc0fc756c07a662c3
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2025-05-25 16:05:19 +0530

    Initialize Password Manager project with basic HTML structure and React components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Maneger from './components/Maneger'
+import Foooter from './components/Foooter'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Maneger />
+      <Foooter />
+    </>
+  )
+}
+
+export default App
